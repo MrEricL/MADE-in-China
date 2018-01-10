db8c505fb332bc3dfc8306ffd63e00ef8ea8d242
MrEricL <eli6@stuy.edu>
2018-01-10 16:03:36 -0500

    finished the java script for closing in rest reg

--- templates/registerrest.js
+++ templates/registerrest.js
@@ -1,10 +1,41 @@
-var sunday = document.getElementByName("sunstatus")
+var sunday = document.getElementById("sunstatus")
+var monday = document.getElementById("monstatus")
+var tuesday = document.getElementById("tuesstatus")
+var wednesday = document.getElementById("wedstatus")
+var thursday = document.getElementById("thurstatus")
+var friday = document.getElementById("fristatus")
+var satday = document.getElementById("satstatus")
 
 
-var checkOC = function(){
-	var sundayStatus = sunday.options[sunday.selectedIndex].text;
-	console.log("closed");
-	if (sundayStatus == "close") this.remove();
+
+
+var checkOC = function(e){
+	
+
+	var hoursname = e.target.name.concat("hour")
+	var hours = document.getElementById(String(hoursname));
+
+	var status = e.target.options[e.target.selectedIndex].value;
+	console.log(hoursname)
+		try{
+		if (status == "close") hours.setAttribute('hidden','true');
+		if (status == "open") hours.removeAttribute("hidden");
+	}
+	catch(errMsg){
+		console.log("Error close the console!!!")
+	}
 }
 
 sunday.addEventListener("change", checkOC);
+
+monday.addEventListener("change", checkOC);
+
+tuesday.addEventListener("change", checkOC);
+
+wednesday.addEventListener("change", checkOC);
+
+thursday.addEventListener("change", checkOC);
+
+friday.addEventListener("change", checkOC);
+
+satday.addEventListener("change", checkOC);
