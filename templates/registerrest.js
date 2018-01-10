4214d31c30a1cdbb6c99644b59bc19f8cef01705
MrEricL <eli6@stuy.edu>
2018-01-10 10:27:07 -0500

    some changes for the js

---
+++ templates/registerrest.js
@@ -0,0 +1,12 @@
+var sunday = document.getElementByName("sunstatus")
+
+
+var checkOC = function(){
+	var sundayStatus = sunday.options[sunday.selectedIndex].text;
+	console.log("closed");
+	if (sundayStatus == "close") this.remove();
+
+
+}
+
+sunday.addEventListener("change", checkOC);
