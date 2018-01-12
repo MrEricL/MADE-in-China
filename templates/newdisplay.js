baa688b2af6db3685c516de775012dadc73de5e0
DimitriyLeks <dimitriyl@DimitriLekssMBP.fios-router.home>
2018-01-11 22:08:23 -0500

    added capacity to add and remove tables

---
+++ templates/newdisplay.js
@@ -0,0 +1,22 @@
+var addButton = document.getElementById("add");
+var removeButton = document.getElementById("remove");
+var list = document.getElementById("selectable");
+
+var selectItem = 8;
+
+var addItem = function(e){
+    selectItem += 1;
+    var nextElement = document.createElement('LI');
+    nextElement.innerHTML = selectItem;
+    list.appendChild(nextElement);
+}
+
+addButton.addEventListener('click', addItem);
+
+var removeItem = function(e){
+    list.removeChild(list.lastChild);
+    selectItem -= 1;
+
+}
+
+removeButton.addEventListener('click', removeItem);
