394d426aa9227bdad5b8d0ddcaa084aee452c6e0
DimitriyLeks <dimitriyl@DimitriLekssMBP.fios-router.home>
2018-01-23 08:55:02 -0500

    added MxN functionality

--- static/newdisplay.js
+++ static/newdisplay.js
@@ -1,27 +1,71 @@
+var alertable = true;
+
+var all = document.getElementById("everything");
 var submitButton = document.getElementById('submit');
 var anewButton = document.getElementById('anew');
-var addButton = document.getElementById("add");
-var removeButton = document.getElementById("remove");
+//var addButton = document.getElementById("add");                               
+//var removeButton = document.getElementById("remove");                         
 var list = document.getElementById("selectable");
+
+var m = document.getElementById("m");
+var mButton = document.getElementById("mButton");
+
+var n = document.getElementById("n");
+//var nButton = document.getElementById("nButton");                             
+
 
 var selectItem = 8;
 
-var addItem = function(e){
-    selectItem += 1;
-    var nextElement = document.createElement('LI');
-    nextElement.innerHTML = "O";
-    list.appendChild(nextElement);
+var addMN = function(e){
+    for(j = 0; j < n.value; j++){
+        for(i = -1; i < m.value; i++){
+            selectItem += 1;
+            var nextElement = document.createElement('LI');
+            nextElement.innerHTML = " ";
+            nextElement.className = "ui-widget-content";
+            //nextElement.style.background = "green";                           
+            nextElement.style.border = "green";
+            if(i == (m.value - 1)){
+                nextElement.style.marginLeft = "1200px";
+                nextElement.style.backgroundColor = "white";
+                nextElement.style.height = "0px";
+                nextElement.style.width = "0px";
+                nextElement.style.marginTop = "0px";
+                nextElement.style.marginBottom = "0px";
+            }
+
+
+            list.appendChild(nextElement);
+
+        }
+    }
+    var finalElement = document.createElement('LI');
+    finalElement.innerHTML = "Start selecting tables?";
+    finalElement.style.width = "1200px";
+    finalElement.style.backgroundColor = "green";
+    list.appendChild(finalElement);
 }
 
-addButton.addEventListener('click', addItem);
+    var mDisable = function(e){
 
-var removeItem = function(e){
-    list.removeChild(list.lastChild);
-    selectItem -= 1;
+	if((m.value > 7) || (n.value > 7)){
+	    mButton.disabled = true;
+	    if(alertable == true){
+		window.alert("7x7 is the largest possible size");
+	    }
+	    alertable = false;
+	}
+	else{
+	    mButton.disabled = false;
+	    alertable = true;
+	}
+    }
 
-}
+all.addEventListener('mouseover', mDisable);
 
-removeButton.addEventListener('click', removeItem);
+mButton.addEventListener('click', addMN);
+
+
 
 var submit = function(e){
     $("#selectable").selectable("destroy");
--- test/newdisplay.js
+++ test/newdisplay.js
@@ -1,27 +1,86 @@
+var alertable = true;
+
+var all = document.getElementById("everything");
 var submitButton = document.getElementById('submit');
 var anewButton = document.getElementById('anew');
-var addButton = document.getElementById("add");
-var removeButton = document.getElementById("remove");
+//var addButton = document.getElementById("add");
+//var removeButton = document.getElementById("remove");
 var list = document.getElementById("selectable");
+
+var m = document.getElementById("m");
+var mButton = document.getElementById("mButton");
+
+var n = document.getElementById("n");
+//var nButton = document.getElementById("nButton");
+
 
 var selectItem = 8;
 
-var addItem = function(e){
+/*var addItem = function(e){
     selectItem += 1;
     var nextElement = document.createElement('LI');
-    nextElement.innerHTML = "O";
+    nextElement.innerHTML = " ";
     list.appendChild(nextElement);
 }
 
-addButton.addEventListener('click', addItem);
+addButton.addEventListener('click', addItem);*/
 
-var removeItem = function(e){
+var addMN = function(e){
+    for(j = 0; j < n.value; j++){
+	for(i = -1; i < m.value; i++){
+	    selectItem += 1;
+	    var nextElement = document.createElement('LI');
+	    nextElement.innerHTML = " ";
+	    nextElement.className = "ui-widget-content";
+	    //nextElement.style.background = "green";
+	    nextElement.style.border = "green";
+	    if(i == (m.value - 1)){
+		nextElement.style.marginLeft = "1200px";
+	        nextElement.style.backgroundColor = "white";
+		nextElement.style.height = "0px";
+		nextElement.style.width = "0px";
+		nextElement.style.marginTop = "0px";
+		nextElement.style.marginBottom = "0px";
+	    }
+	    
+
+	    list.appendChild(nextElement);
+
+	}
+    }
+    var finalElement = document.createElement('LI');
+    finalElement.innerHTML = "Start selecting tables?";
+    finalElement.style.width = "1200px";
+    finalElement.style.backgroundColor = "green";
+    list.appendChild(finalElement);
+}
+
+var mDisable = function(e){
+
+    if((m.value > 7) || (n.value > 7)){
+	mButton.disabled = true;
+	if(alertable == true){
+	    window.alert("7x7 is the largest possible size");
+	}
+	alertable = false;
+    }
+    else{
+	mButton.disabled = false;
+	alertable = true;
+    }
+}
+
+all.addEventListener('mouseover', mDisable);
+
+mButton.addEventListener('click', addMN);
+    
+/*var removeItem = function(e){
     list.removeChild(list.lastChild);
     selectItem -= 1;
 
 }
 
-removeButton.addEventListener('click', removeItem);
+removeButton.addEventListener('click', removeItem);*/
 
 var submit = function(e){
     $("#selectable").selectable("destroy");
@@ -35,6 +94,9 @@ var anew = function(e){
 
 anewButton.addEventListener("click", anew);
 
+//Selecting
+$(".ui-selecting").css("background", "#000000");
+//
 
 //NEW STUFF
 
